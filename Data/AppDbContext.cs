c3178e813ebd1f13421933b49ac38ff763a24602
eimwe <prokopenkoserge@gmail.com>
2026-06-19 19:24:47 +0500

    Added db context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using limbo.Models;
+
+namespace limbo.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique()
+            .HasDatabaseName("idx_users_email");
+    }
+}
