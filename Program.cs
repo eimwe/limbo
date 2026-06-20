b7e6f108a0ecadfdd3bc30f4fbe2cdf84165015a
eimwe <prokopenkoserge@gmail.com>
2026-06-20 16:50:07 +0500

    Updated main program file

--- Program.cs
+++ Program.cs
@@ -1,29 +1,67 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using limbo.Data;
+using limbo.Middleware;
+using limbo.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var dbHost     = Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost";
+var dbPort     = Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
+var dbName     = Environment.GetEnvironmentVariable("DB_NAME") ?? "limbo";
+var dbUser     = Environment.GetEnvironmentVariable("DB_USER") ?? "postgres";
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "";
+
+var connectionString =
+    $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword}";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var emailPassword = Environment.GetEnvironmentVariable("EMAIL_PASSWORD") ?? "";
+var emailUser     = Environment.GetEnvironmentVariable("EMAIL_USER") ?? "";
+
+builder.Services.AddSingleton(new EmailSettings
+{
+    Host     = builder.Configuration["Smtp:Host"] ?? "smtp.gmail.com",
+    Port     = int.Parse(builder.Configuration["Smtp:Port"] ?? "587"),
+    User     = emailUser,
+    Password = emailPassword,
+    From     = emailUser
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/auth/login";
+        options.ExpireTimeSpan = TimeSpan.FromHours(24);
+        options.SlidingExpiration = true;
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddScoped<EmailService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
 }
 
-app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+
+app.UseMiddleware<UserStatusMiddleware>();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Auth}/{action=Login}/{id?}");
 
 app.Run();
